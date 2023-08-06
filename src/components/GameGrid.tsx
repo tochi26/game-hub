779deee162203320432adab202cd9dbe0704c5e8
Tochukwu <nzewitochukwu26@gmail.com>
2023-08-07 00:09:04 +0100

    Create a generic data fetching hook

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -7,7 +7,7 @@ import { GameCardSkeleton } from "./GameCardSkeleton";
 import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
-  const { games, error, isloading } = useGames();
+  const { data, error, isLoading } = useGames();
   const skeletons = [1, 2, 3, 4, 5, 6];
 
   return (
@@ -18,13 +18,13 @@ const GameGrid = () => {
         padding="10px"
         spacing={10}
       >
-        {isloading &&
+        {isLoading &&
           skeletons.map((skeleton) => (
             <GameCardContainer>
               <GameCardSkeleton key={skeleton} />
             </GameCardContainer>
           ))}
-        {games.map((game) => (
+        {data.map((game) => (
           <GameCardContainer>
             <GameCard key={game.id} game={game} />
           </GameCardContainer>
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,7 +1,4 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
-
+import useData from "./useData";
 
 export interface Platform {
     id: number;
@@ -17,37 +14,6 @@ export interface Game {
     metacritic: number;
   }
   
-  interface FetchGamesResponse {
-    count: number;
-    results: Game[];
-  }
-
-const useGames = () => {
-    const [games, setGames] = useState<Game[]>([]);
-    const [error, setError] = useState("");
-    const [isloading, setLoading] =useState(false);
-  
-    useEffect(() => {
-        const controllers = new AbortController();
-      
-      setLoading(true);
-      apiClient
-        .get<FetchGamesResponse>("/games", {signal: controllers.signal})
-        .then((res) => {
-          setGames(res.data.results);
-          setLoading(false);
-        })
-        .catch((err) => {
-            if (err instanceof CanceledError) return;
-            setError(err.message)
-            setLoading(false);
-          });
-
-        return () => controllers.abort();
-    }, []);
-
-    return { games, error, isloading };
-
-}
+const useGames = () => useData<Game>('/games');
 
 export default useGames;
