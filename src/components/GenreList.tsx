485fe0e270a2c5cafa596d3596301cfeb862424b
Tochukwu <nzewitochukwu26@gmail.com>
2023-08-07 00:39:58 +0100

    Display genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,25 @@
-import useGenres from "../hooks/useGenres";
+import React from "react";
+import useGenres, { Genre } from "../hooks/useGenres";
+import useData from "../hooks/useData";
+import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import getCroppedImageUrl from "../services/image-url";
 
 export const GenreList = () => {
-  const { data } = useGenres();
+  const { data } = useData<Genre>("/genres");
   return (
-    <ul>
+    <List>
       {data.map((genre) => (
-        <li key={genre.id}>{genre.name}</li>
+        <ListItem key={genre.id} paddingY="5px">
+          <HStack>
+            <Image
+              boxSize="32px"
+              borderRadius={8}
+              src={getCroppedImageUrl(genre.image_background)}
+            />
+            <Text fontSize="lg">{genre.name}</Text>
+          </HStack>
+        </ListItem>
       ))}
-    </ul>
+    </List>
   );
 };
